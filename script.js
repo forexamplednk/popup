62587afd4b5382694736f9b9c215501c3daf7c46
Darya Konchyts <forExampleDNK@proton.me>
2024-10-01 13:24:53 +0300

    add: create function

--- script.js
+++ script.js
@@ -8,19 +8,60 @@ let popup = function() {
 Проверять наличие элементов с атрибутом "data-popup"
 */
   let create = function() {
+    let elemBackground = document.createElement('div');
+    elemBackground.classList.add('popup__background');
 
+    let elem = document.createElement('div');
+    elem.classList.add('popup');
+
+    let buttonClose = document.createElement('button');
+    buttonClose.classList.add('popup__close');
+    buttonClose.innerText = '+';
+
+    let elemBlock = document.createElement('div');
+    elemBlock.classList.add('popup__block');
+
+    let elemContent = document.createElement('div');
+    elemContent.classList.add('popup__content');
+
+    elemBlock.append(elemContent);
+    elem.append(buttonClose, elemBlock);
+
+    buttonClose.addEventListener('click', remove);
+    elem.addEventListener('click', function(e) {
+      if (!e.target.classList.contains('popup')) return;
+      remove();
+    });
+
+    return [elemBackground, elem];
   };
 
   let remove = function() {
 
   };
 
-  let show = function() {
-
+  let show = function(content = null) {
+    remove();
+    create();
   };
 
-  let action = function() {
+  let action = function(e) {
+    e.preventDefault();
 
+    let elem = e.target;    
+
+    if (!('popup' in elem.dataset)) return;
+    if ('popupId' in elem.dataset) {
+      let id = elem.dataset.popupId;
+      let contentById = document.querySelector(`#${id}`);
+       // console.log(contentById);
+      if (!contentById) return;
+      
+      contentById = contentById.innerHTML;
+      show(contentById);
+      
+    }
+    //console.dir(elem)
   };
  
   let elems = document.querySelectorAll('[data-popup]');
